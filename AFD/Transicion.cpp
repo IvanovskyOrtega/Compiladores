fbfbc46839fd907865e70dd938714d444778ccc8
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-26 16:58:30 -0500

    Clase Transicion

---
+++ AFD/Transicion.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include "Transicion.hpp"
+
+using namespace std;
+
+Transicion::Transicion(int estado1, int estado2, char simbolo){
+    estadoActual = estado1;
+    estadoDeTransicion = estado2;
+    simboloDeTransicion = simbolo;
+  };
