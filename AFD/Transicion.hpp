9e963099680d0303f30ccdbf2b781b8402727dbe
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-26 16:56:52 -0500

    Transicion.hpp

---
+++ AFD/Transicion.hpp
@@ -0,0 +1,16 @@
+#ifndef _TRANSICION_HPP_
+#define _TRANSICION_HPP_
+
+#include <vector>
+
+using namespace std;
+
+class Transicion{
+public:
+  int estadoActual;
+  int estadoDeTransicion;
+  char simboloDeTransicion;
+  Transicion(int estado1, int estado2, char simbolo);
+};
+
+#endif
