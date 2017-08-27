e55c9a6d6df7206b0d18943811f4d7c5e88358cc
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-27 13:59:21 -0500

    AFD.hpp

---
+++ Practica-1-AFD-y-AFND/AFD.hpp
@@ -0,0 +1,36 @@
+#ifndef _AFD_HPP_
+#define _AFD_HPP_
+
+#include <vector>
+#include "Estado.hpp"
+#include "Transicion.hpp"
+
+
+using namespace std;
+class AFD{
+private:
+  vector<Estado*> afd_estados;
+  Estado* afd_estadoInicial;
+  vector<char> afd_alfabeto;
+  vector<Transicion*> afd_tablaDeTransiciones;
+public:
+  void toString();
+  vector<Estado*> obtenerEstados();
+  vector<Transicion*> obtenerTransiciones();
+  Estado* obtenerEstadoInicial();
+  AFD();
+  AFD(vector<Estado*> estados, Estado* estadoInicial, vector<char> alfabeto, vector<Transicion*> tablaDeTransiciones);
+};
+
+#endif
+
+/**
+  REPORTE:
+    INTRODUCCIÓN
+    DESARROLLO
+    RESULTADOS
+    CONCLUSIONES
+  PRÁCTICA 1:
+    DISEÑAR E IMPLEMENTAR LAS CLASES AFN Y AFD
+
+**/
