c15809d52ab6d537e1590ed3dc8689ea2b4833f4
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-26 20:39:28 -0500

    Se agregaron métodos para probar funcionamiento

--- AFD/Pruebas.cpp
+++ AFD/Pruebas.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 #include "Pruebas.hpp"
+#include "AFD.hpp"
 #include <vector>
 
 using namespace std;
@@ -27,7 +30,7 @@ vector<char> Pruebas::definirAlfabeto(int numeroDeSimbolos){
   vector<char> alfabetoAux(numeroDeSimbolos); // Usamos un arreglo auxiliar
   char simbolo;
   for(int i = 0; i < numeroDeSimbolos; i++){    // Loop para inicializar el vector
-    cout << "Ingresa el simbolo " << i << ": ";
+    cout << "\nIngresa el simbolo " << i << ": ";
     cin >> simbolo;
     alfabetoAux[i] = simbolo;
   }
@@ -55,7 +58,7 @@ vector<Estado*> Pruebas::agregarEstados(int numeroDeEstados){
       }
     }
     else{
-      cout << "Estado inicial q0" << endl;
+      cout << "\nEstado inicial q0" << endl;
       estados.reserve(1);
       estados.push_back(new Estado(i,false));
     }
@@ -72,18 +75,62 @@ vector<Transicion*> Pruebas::definirTransiciones(int numeroDeTransiciones, vecto
     int estado1;
     int estado2;
     char simbolo;
-    cout << "Ingresa el numero del primer estado: ";
+    cout << "\nIngresa el numero del primer estado: ";
     cin >> estado1;
-    cout << "Ingresa el numero del segundo estado: ";
+    cout << "\nIngresa el numero del segundo estado: ";
     cin >> estado2;
-    cout << "Ingresa el simbolo de transicion: ";
+    cout << "\nIngresa el simbolo de transicion: ";
     cin >> simbolo;
     while(!Pruebas::buscarSimbolo(alfabeto, simbolo)){
-        cout << "Ingresa un simbolo que pertenezca al alfabeto: ";
+        cout << "\nIngresa un simbolo que pertenezca al alfabeto: ";
         cin >> simbolo;
     }
     transiciones.reserve(1);
     transiciones.push_back(new Transicion(estado1,estado2,simbolo));
   }
   return transiciones;
+}
+
+bool Pruebas::cambiarDeEstado(vector<Estado*> estados, vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, string cadena, char simbolo, int indice){
+  int i;
+  int k = tablaDeTransiciones.size();
+  if(indice < (int)cadena.size()){
+    indice++;
+    cout << endl;
+    for(i = 0; i < k; i++){
+        if(tablaDeTransiciones[i]->estadoActual == estadoActual->numeroDeEstado){
+          if(tablaDeTransiciones[i]->simboloDeTransicion == simbolo){
+            cout << "\tδ(q" << estadoActual->numeroDeEstado << "," << simbolo << ")";
+            cout << " = q" << tablaDeTransiciones[i]->estadoDeTransicion << endl;
+            return cambiarDeEstado(estados, tablaDeTransiciones, estados[tablaDeTransiciones[i]->estadoDeTransicion], cadena, cadena[indice],indice);
+          }
+
+        }
+    }
+    cout << "\tδ(q" << estadoActual->numeroDeEstado << "," << simbolo << ")";
+    cout << " = M" << endl;
+    return false;
+  }
+  else
+    return estadoActual->esFinal;
+}
+
+bool Pruebas::iniciarPrueba(AFD automataDePrueba){
+  string cadenaDePrueba;
+  cout << "Ingresa una cadena de prueba: ";
+  cin >> cadenaDePrueba;
+  return cambiarDeEstado(automataDePrueba.obtenerEstados(), automataDePrueba.obtenerTransiciones(), automataDePrueba.obtenerEstadoInicial(), cadenaDePrueba, cadenaDePrueba[0],0);
+}
+
+void Pruebas::probarCadenas(AFD automataDePrueba){
+  char opcion;
+  automataDePrueba.toString();
+  while(opcion != 'n'){
+    if(iniciarPrueba(automataDePrueba))
+      cout << "Cadena valida" << endl;
+    else
+      cout << "Cadena invalida" << endl;
+    cout << "Desea probar otra cadena? (s/n)" << endl;
+    cin >> opcion;
+  }
 }
