3542cf6f4e103fad75c8418218a52b7c4ed8f258
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-26 20:38:03 -0500

    AFD.hpp

--- AFD/AFD.hpp
+++ AFD/AFD.hpp
@@ -14,6 +14,9 @@ private:
   vector<Transicion*> afd_tablaDeTransiciones;
 public:
   void toString();
+  vector<Estado*> obtenerEstados();
+  vector<Transicion*> obtenerTransiciones();
+  Estado* obtenerEstadoInicial();
   AFD();
   AFD(vector<Estado*> estados, Estado* estadoInicial, vector<char> alfabeto, vector<Transicion*> tablaDeTransiciones);
 };
