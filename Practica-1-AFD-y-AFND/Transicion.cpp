7ce16d45fb4c623f7a44e19e35cab223e200534f
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-27 14:01:08 -0500

    Ya incluye constructor para transición epsilon de un AFND

---
+++ Practica-1-AFD-y-AFND/Transicion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "Transicion.hpp"
+
+using namespace std;
+
+Transicion::Transicion(int estado1, int estado2, char simbolo){
+    estadoActual = estado1;
+    estadoDeTransicion = estado2;
+    simboloDeTransicion = simbolo;
+};
+
+Transicion::Transicion(int estado1, int estado2, char simbolo, bool esEpsilon){
+    estadoActual = estado1;
+    estadoDeTransicion = estado2;
+    simboloDeTransicion = simbolo;
+    esTransicionEpsilon = esEpsilon;
+};
