d811cdfa74e6c428be8abb1b8ed44e72b71a6a27
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-31 22:42:55 -0500

    AFN.hpp

--- Practica-1-AFD-y-AFND/AFN.hpp
+++ Practica-1-AFD-y-AFND/AFN.hpp
@@ -1,25 +1,13 @@
 #ifndef _AFN_HPP_
 #define _AFN_HPP_
 
-#include <vector>
-#include "Estado.hpp"
-#include "Transicion.hpp"
+#include "Automata.hpp"
 
-using namespace std;
-
-class AFN{
-private:
-  vector<Estado*> afn_estados;
-  vector<char> afn_alfabeto;
-  Estado* afn_estadoInicial;
-  vector<Transicion*> afn_tablaDeTransiciones;
+class AFN: public Automata{
 public:
-  void toString();
-  vector<Estado*> obtenerEstados();
-  vector<Transicion*> obtenerTransiciones();
-  Estado* obtenerEstadoInicial();
-  AFN(vector<Estado*> estados, vector<char> alfabeto, Estado* estadoInicial, vector<Transicion*> tablaDeTransiciones);
-  AFN();
+    AFN(std::string archivo, std::string tipo):Automata(archivo, tipo){};
+    AFN(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, std::vector<Transicion*> tablaDeTransiciones):Automata(estados, alfabeto, estadoInicial, tablaDeTransiciones){};
+    bool cambiarDeEstadoAFN(std::vector<Estado*> estados, std::vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, std::string cadena, char simbolo, int indice);
 };
 
 #endif
