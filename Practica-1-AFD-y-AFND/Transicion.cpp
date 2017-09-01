358b327e1eea55691f26835640ec2f6cbd945a0f
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-31 22:47:44 -0500

    Clase Transicion

--- Practica-1-AFD-y-AFND/Transicion.cpp
+++ Practica-1-AFD-y-AFND/Transicion.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
-#include <vector>
 #include "Transicion.hpp"
-
-using namespace std;
 
 Transicion::Transicion(int estado1, int estado2, char simbolo){
     estadoActual = estado1;
