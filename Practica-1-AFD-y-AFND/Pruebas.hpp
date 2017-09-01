8e14be2b04db4589d0f7a5364ec8233dedec25d2
Ivanovsky <ivanovskyortega@gmail.com>
2017-08-31 22:43:37 -0500

    Pruebas.hpp

--- Practica-1-AFD-y-AFND/Pruebas.hpp
+++ Practica-1-AFD-y-AFND/Pruebas.hpp
@@ -1,30 +1,17 @@
 #ifndef _PRUEBAS_HPP_
 #define _PRUEBAS_HPP_
 
-#include "AFD.hpp"
-#include "AFN.hpp"
+#include <iostream>
+#include <vector>
 #include "Estado.hpp"
 #include "Transicion.hpp"
-#include <vector>
-
-using namespace std;
 
 class Pruebas{
 public:
   Pruebas();
-  bool buscarSimbolo(vector<char> alfabeto, char simbolo);
-  vector<char> definirAlfabeto(int numeroDeSimbolos);
-  vector<Estado*> agregarEstados(int numeroDeEstados);
-  vector<Transicion*> definirTransicionesAFD(int numeroDeTransiciones, vector<char> alfabeto);
-  vector<Transicion*> definirTransicionesAFN(int numeroDeTransiciones, vector<char> alfabeto);
-  bool cambiarDeEstadoAFD(vector<Estado*> estados, vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, string cadena, char simbolo, int indice);
-  bool cambiarDeEstadoAFN(vector<Estado*> estados, vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, string cadena, char simbolo, int indice);
-  bool iniciarPrueba(AFD automataDePrueba);
-  bool iniciarPrueba(AFN automataDePrueba);
-  void probarCadenas(AFD automataDePrueba);
-  void probarCadenas(AFN automataDePrueba);
-  void crearAFD();
-  void crearAFN();
+  bool buscarSimbolo(std::vector<char> alfabeto, char simbolo);
+  std::vector<char> definirAlfabeto(int numeroDeSimbolos);
+  std::vector<Estado*> agregarEstados(int numeroDeEstados);
 };
 
 #endif // _PRUEBAS_HPP_
