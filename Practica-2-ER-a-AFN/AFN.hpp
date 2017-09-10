8b616822555f9e638374a108fb1e818376d93f9b
Ivanovsky <ivanovskyortega@gmail.com>
2017-09-10 10:53:28 -0500

    Se agregó constructor

--- Practica-2-ER-a-AFN/AFN.hpp
+++ Practica-2-ER-a-AFN/AFN.hpp
@@ -8,6 +8,7 @@ public:
     AFN():Automata(){};
     AFN(std::string archivo, std::string tipo):Automata(archivo, tipo){};
     AFN(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, std::vector<Transicion*> tablaDeTransiciones):Automata(estados, alfabeto, estadoInicial, tablaDeTransiciones){};
+    AFN(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, Estado* estadoFinal, std::vector<Transicion*> tablaDeTransiciones):Automata(estados, alfabeto, estadoInicial, tablaDeTransiciones){};
     bool cambiarDeEstadoAFN(std::vector<Estado*> estados, std::vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, std::string cadena, char simbolo, int indice);
 
 };
