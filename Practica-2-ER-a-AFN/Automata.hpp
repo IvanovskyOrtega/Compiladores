b9d635ab18a25f333e3c0c4e8a485710778ff583
Ivanovsky <ivanovskyortega@gmail.com>
2017-09-10 10:54:05 -0500

    Se agregó el atributo estado final

---
+++ Practica-2-ER-a-AFN/Automata.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef _AUTOMATA_HPP_
+#define _AUTOMATA_HPP
+
+#include "Estado.hpp"
+#include "Transicion.hpp"
+#include <vector>
+#include <string>
+#include <iostream>
+
+class Automata{
+public:
+    std::vector<Estado*> automata_estados;
+    std::vector<char> automata_alfabeto;
+    Estado* automata_estadoInicial;
+    Estado* automata_estadoFinal;
+    std::vector<Transicion*> automata_tablaDeTransiciones;
+    Automata();
+    Automata(std::string archivo, std::string tipo);
+    Automata(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, std::vector<Transicion*> tablaDeTransiciones);
+    Automata(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, Estado* estadoFinal, std::vector<Transicion*> tablaDeTransiciones);
+    std::vector<Estado*> obtenerEstados();
+    std::vector<Transicion*> obtenerTablaDeTransiciones();
+    std::vector<char> obtenerAlfabeto();
+    Estado* obtenerEstadoInicial();
+    Estado* obtenerEstadoFinal();
+    void toString(std::string tipo);
+};
+
+#endif
