521a3564f9b9005883009fe0c9819d6bb14493d4
Ivanovsky <ivanovskyortega@gmail.com>
2017-09-10 10:52:20 -0500

    Se agregó constructor

---
+++ Practica-2-ER-a-AFN/AFN.hpp
@@ -0,0 +1,15 @@
+#ifndef _AFN_HPP_
+#define _AFN_HPP_
+
+#include "Automata.hpp"
+
+class AFN: public Automata{
+public:
+    AFN():Automata(){};
+    AFN(std::string archivo, std::string tipo):Automata(archivo, tipo){};
+    AFN(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, std::vector<Transicion*> tablaDeTransiciones):Automata(estados, alfabeto, estadoInicial, tablaDeTransiciones){};
+    bool cambiarDeEstadoAFN(std::vector<Estado*> estados, std::vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, std::string cadena, char simbolo, int indice);
+
+};
+
+#endif
