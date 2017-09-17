5aaae7e82ed5f9a37bee002a310af4d13f338f8c
ivanovsky <ivanovsky@ivanovsky-ubuntu>
2017-09-17 18:34:02 -0500

    AFN.hpp

--- Practica-2-ER-a-AFN/AFN.hpp
+++ Practica-2-ER-a-AFN/AFN.hpp
@@ -10,7 +10,18 @@ public:
     AFN(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, std::vector<Transicion*> tablaDeTransiciones):Automata(estados, alfabeto, estadoInicial, tablaDeTransiciones){};
     AFN(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, Estado* estadoFinal, std::vector<Transicion*> tablaDeTransiciones):Automata(estados, alfabeto, estadoInicial, tablaDeTransiciones){};
     bool cambiarDeEstadoAFN(std::vector<Estado*> estados, std::vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, std::string cadena, char simbolo, int indice);
-
+    void agregarTransicion(AFN* afn, int estado1, int estado2, char simbolo, bool esEpsilon);
+    void renumerarEstados(AFN* subafn, int numero);
+    void agregarTransiciones(AFN* afn, std::vector<Transicion*> tablaDeTransiciones);
+    void agregarTransicionEpsilon(AFN* afn, Estado* estado1,Estado* estado2);
+    void agregarEstados(AFN* afn, std::vector<Estado*> estados);
+    void concatenar(AFN* afn, char simbolo, bool esEpsilon);
+    void unir(AFN* afn, AFN subafn);
+    void eliminarEstadoFinal(AFN* afn);
+    void aplicarCerraduraKleenAFN(AFN *afn);
+    void aplicarCerraduraKleen(AFN* afn, char c, bool esEpsilon);
+    void aplicarCerraduraPositivaAFN(AFN *afn);
+    void aplicarCerraduraPositiva(AFN* afn, char c, bool esEpsilon);
 };
 
 #endif
