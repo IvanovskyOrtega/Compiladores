c2d7a3c6bb6e11c44ff9f035309251f93c3ebee7
ivanovsky <ivanovsky@ivanovsky-ubuntu>
2017-09-17 18:37:33 -0500

    Ya se puede crear AFN a partir de ER

--- Practica-2-ER-a-AFN/main.cpp
+++ Practica-2-ER-a-AFN/main.cpp
@@ -10,10 +10,13 @@ int main(int argc, char *argv[])
   char tipoAutomata, opcion;
 	PruebasAFD *p1 = new PruebasAFD();
 	PruebasAFN *p2 = new PruebasAFN();
+  string cadena;
+  AFN afn;
   while(opcion != 'n'){
      cout << "Ingrese el tipo de automata que desea crear:" << endl;
      cout << "1\tAutomata Finito Determinista (AFD)" << endl;
      cout << "2\tAutomata Finito No Determinista (AFN)" << endl;
+     cout << "3\tER a AFN (Contrucciones de Thompson)" << endl;
      cout << "Ingrese su seleccion: ";
      cin >> tipoAutomata;
      switch(tipoAutomata){
@@ -21,6 +24,12 @@ int main(int argc, char *argv[])
         p1->crearAFD(); break;
        case '2':
         p2->crearAFN(); break;
+       case '3':
+        cout << "Ingresa una ER: ";
+        cin >> cadena;
+        afn = p2->convertirERaAFN(cadena);
+        p2->probarCadenasAFN(afn);
+        break;
        default:
         cout << "Opcion no valida" << endl;
      }
