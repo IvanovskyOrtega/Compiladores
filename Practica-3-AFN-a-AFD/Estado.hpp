ce2e61e6fc1cf4d860eac3eedbcee2486d0249c9
ivanovsky <ivanovsky@ivanovsky-ubuntu>
2017-10-03 22:24:43 -0500

    Estado.hpp

---
+++ Practica-3-AFN-a-AFD/Estado.hpp
@@ -0,0 +1,12 @@
+#ifndef _ESTADO_HPP_
+#define _ESTADO_HPP_
+
+class Estado {
+public:
+  int numeroDeEstado;
+  bool esFinal;
+  Estado();
+  Estado(int numDeEstado, bool tipo);
+};
+// Estado.hpp
+#endif
