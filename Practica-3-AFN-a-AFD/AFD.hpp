e31c91848f5c8f14e880f4049aa9e7ea41059d8d
ivanovsky <ivanovsky@ivanovsky-ubuntu>
2017-10-03 22:20:21 -0500

    AFD.cpp

---
+++ Practica-3-AFN-a-AFD/AFD.hpp
@@ -0,0 +1,24 @@
+#ifndef _AFD_HPP_
+#define _AFD_HPP_
+
+#include "Automata.hpp"
+
+class AFD : public Automata{
+public:
+  AFD(std::string archivo, std::string tipo):Automata(archivo, tipo){};
+  AFD(std::vector<Estado*> estados, std::vector<char> alfabeto, Estado* estadoInicial, std::vector<Transicion*> tablaDeTransiciones):Automata(estados, alfabeto, estadoInicial, tablaDeTransiciones){};
+  bool cambiarDeEstadoAFD(std::vector<Estado*> estados, std::vector<Transicion*> tablaDeTransiciones, Estado* estadoActual, std::string cadena, char simbolo, int indice);
+};
+
+#endif
+
+/**
+  REPORTE:
+    INTRODUCCIÓN
+    DESARROLLO
+    RESULTADOS
+    CONCLUSIONES
+  PRÁCTICA 1:
+    DISEÑAR E IMPLEMENTAR LAS CLASES AFN Y AFD
+
+**/
