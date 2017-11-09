f7fc3f5afe1e36816adcd943d5f09eb32b6c8e7f
Ivanovsky <ivanovskyortega@gmail.com>
2017-11-08 22:27:18 -0600

    Producciones

---
+++ Practica-6-LL1/Produccion.java
@@ -0,0 +1,10 @@
+package ll1;
+
+public class Produccion {
+    public char simbolo;
+    public String produccion;
+    Produccion(char simbolo, String produccion){
+        this.simbolo = simbolo;
+        this.produccion = produccion;
+    }
+}
