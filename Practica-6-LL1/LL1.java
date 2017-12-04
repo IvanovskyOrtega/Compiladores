8897069e494aad2f2714a258c00a55e787aa6d26
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-12-03 21:27:10 -0600

    Ya solo queda generar la tabla

--- Practica-6-LL1/LL1.java
+++ Practica-6-LL1/LL1.java
@@ -1,22 +1,27 @@
 package ll1;
 
-public class LL1 {
+public class LL1 extends Funciones{
     public static void main(String[] args) {
         Funciones f = new Funciones();
         f.leerReglasDeProduccion();
+        System.out.println("Axioma: "+f.axioma);
         for (Character c: f.noTerminales) {
             f.obtenerPrimeros(c);
         }
-        for(PS p : f.primeros){
-            System.out.println("Primeros de "+p.simbolo+":");
-            System.out.println(p.primeros.toString());
+        for(Character c : f.noTerminales){
+            if(f.conjuntoPrimeros.containsKey(c)){
+                System.out.println("Primeros de "+c+":");
+                System.out.println(f.conjuntoPrimeros.get(c).toString());
+            };
         }
         for (Character c: f.noTerminales) {
             f.obtenerSiguientes(c);
         }
-        for(PS p : f.siguientes){
-            System.out.println("Siguientes de "+p.simbolo+":");
-            System.out.println(p.siguientes.toString());
+        for(Character c: f.noTerminales){
+            if(f.conjuntoSiguientes.containsKey(c)){
+                System.out.println("Siguientes de "+c+":");
+                System.out.println(f.conjuntoSiguientes.get(c).toString());
+            };
         }
     }
 }
