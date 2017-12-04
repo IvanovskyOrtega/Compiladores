2fdad4107e734c68cf993a1bb1b28454cb594033
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-12-03 21:26:39 -0600

    Ya funciona bien :)
    Se implementa el uso de un HashMap para los conjuntos de primeros y siguientes.

--- Practica-6-LL1/Funciones.java
+++ Practica-6-LL1/Funciones.java
@@ -1,40 +1,35 @@
 package ll1;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Stream;
 
 public class Funciones {
-    public PS aux = new PS();
-    public char axioma;
-    public Set<Character> terminales = new HashSet<Character>();
-    public Set<Character> noTerminales = new HashSet<Character>();
-    public Set<Produccion> reglasDeProduccion = new HashSet<Produccion>();
-    public Set<PS> primeros = new HashSet<PS>();
-    public Set<PS> siguientes = new HashSet<PS>();
-    public Set<Character> listaDeEspera = new HashSet<Character>();
+    Character EPSILON = 'ε';
+    Character axioma;
+    Set<Character> terminales = new HashSet<Character>();
+    Set<Character> noTerminales = new HashSet<Character>();
+    ArrayList<Produccion> reglasDeProduccion = new ArrayList<Produccion>();
+    Map<Character,Set<Character>> conjuntoPrimeros = new HashMap<Character,Set<Character>>();
+    Map<Character,Set<Character>> conjuntoSiguientes = new HashMap<Character,Set<Character>>();
     Scanner input = new Scanner(System.in);
-
-    /**
-     * Esta función lee desde consola las reglas de producción de la Gramática Libre de Contexto.
-     *
-     */
-    public void leerReglasDeProduccion(){
+    
+    void leerReglasDeProduccion(){
         List<String> lineasArchivo;
         Produccion p;
         System.out.println("Ingresa el nombre del archivo .txt que contiene la gramática (sin extensión):\t");
         lineasArchivo = leerArchivo(input.nextLine()+".txt");
         String linea;
         axioma = lineasArchivo.get(0).charAt(0);
+        System.out.println("Axioma: "+lineasArchivo.get(0));
         linea = lineasArchivo.get(1);
         for (int i = 0; i < linea.length() ; i+=2) {
             noTerminales.add(linea.charAt(i));
         }
         linea = lineasArchivo.get(2);
         for (int i = 0; i < linea.length() ; i+=2) {
-            terminales.add(linea.charAt(i));
+            if(linea.charAt(i) != EPSILON){
+                terminales.add(linea.charAt(i));
+            }
         }
         for(int i = 3; i < lineasArchivo.size(); i++){
             linea  = lineasArchivo.get(i);
@@ -44,153 +39,140 @@ public class Funciones {
     }
 
 
-    public List<String> leerArchivo(String nombreDelArchivo) {
-        FileInputStream fstream = null;
-        List<String> lineasArchivo = new ArrayList<String>();
+    List<String> leerArchivo(String nombreDelArchivo) {
+        List<String> lineasArchivo = new ArrayList<>();
         try {
-            fstream = new FileInputStream("test/"+nombreDelArchivo);
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-            String linea;
-            while ((linea = br.readLine()) != null){
-                lineasArchivo.add(linea);
-                System.out.println (linea);
+            FileInputStream fstream = new FileInputStream("test/"+nombreDelArchivo);
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(fstream,"UTF-8"))){
+                String linea;
+                while ((linea = br.readLine()) != null){
+                    if (linea.startsWith("\uFEFF")){ 
+                        linea = linea.substring(1); 
+                    }
+                    lineasArchivo.add(linea);
+                    System.out.println (linea);
+                }
             }
-            br.close();
         } catch (IOException e) {
-            e.printStackTrace();
         }
         return lineasArchivo;
     }
 
-    public Set<Character> obtenerPrimeros(Character c){
-        char posiblePrimero;
-        Set<Character> res = new HashSet<Character>();
-        PS primerosDeC = new PS();
-        primerosDeC.simbolo = c;
-        int index = 1;
-        for (Produccion p:reglasDeProduccion) {
-            if(p.simbolo == c){
-                posiblePrimero = p.produccion.charAt(0);
-                if(terminales.contains(posiblePrimero)){
-                    primerosDeC.primeros.add(posiblePrimero);
-                    res.add(posiblePrimero);
+    Set<Character> obtenerPrimeros(Character c){
+        if(conjuntoPrimeros.containsKey(c)){
+            return conjuntoPrimeros.get(c);
+        }
+        Set<Character> primeros = new HashSet<Character>();
+        
+        // Si es un terminal, entonces se agrega y termina la funcion.
+        if(terminales.contains(c)){
+            primeros.add(c);
+            conjuntoPrimeros.put(c, primeros);
+            return primeros;
+        }
+        
+        String produccion;
+        Character simboloProduccion;
+        Character productor;
+        for(Produccion p : reglasDeProduccion){
+            produccion = p.produccion;
+            productor = p.simbolo;
+            if(productor.equals(c)){
+                for (int i = 0; i < produccion.length(); i++) {
+                    simboloProduccion = produccion.charAt(i);
+                
+                    // Epsilon se agrega a primeros
+                    if(simboloProduccion.equals(EPSILON)){
+                        primeros.add(EPSILON);
+                        break;
+                    }
+                
+                    // De lo contrario, si es un no terminal, se calculan su
+                    // conjunto de primeros.
+                    Set<Character> primerosDeS = obtenerPrimeros(simboloProduccion);
+                
+                    // Si no contiene a epsilon, se agrega el subconjunto a primeros
+                    if(!primerosDeS.contains(EPSILON)){
+                        primeros.addAll(primerosDeS);
+                        break;
+                    }
+                
+                    // De lo contrario, si contiene epsilon, se agregan todos los
+                    // elementos a excepcion de epsilon.
+                    primeros.addAll(primerosDeS);
+                    primeros.remove(EPSILON);
                 }
-                else  if(noTerminales.contains(posiblePrimero)){
-                    if(yaSeCalculoPrimeros(posiblePrimero)){
-                        if(aux.primeros.contains('Ø') && index < p.produccion.length()){
-                            if(terminales.contains(p.produccion.charAt(index))){
-                                primerosDeC.primeros.add(p.produccion.charAt(index));
-                                res.add(p.produccion.charAt(index));
-                            }
-                            else{
-                                Set<Character> aux = new HashSet<Character>();
-                                aux = obtenerPrimeros(p.produccion.charAt(index));
-                                res.addAll(aux);
-                                primerosDeC.primeros.addAll(aux);
-                                index++;
-                            }
+                conjuntoPrimeros.put(c, primeros);
+            }
+        }
+        return primeros;
+    }
+    
+    Set<Character> obtenerSiguientes(Character c){
+        if(conjuntoSiguientes.containsKey(c)){
+            return conjuntoSiguientes.get(c);
+        }
+        Set<Character> siguientes = new HashSet<>();
+        
+        // Caso especial
+        if(c.equals(axioma)){
+            siguientes.add('$');
+        }
+        
+        // Se analizan todas las producciones que contengan a c
+        String produccion;
+        Character productor;
+        Character simboloProduccion;
+        int posicionSimbolo;
+        int sigPosicion;
+        for(Produccion p : reglasDeProduccion){
+            produccion = p.produccion;
+            
+            // Obtenemos la primera ocurrencia en la cadena.
+            posicionSimbolo = produccion.indexOf(c);
+            if(posicionSimbolo != -1){
+                // Asignamos la siguiente posicion
+                sigPosicion = posicionSimbolo + 1;
+            
+                // Se buscan los simbolo siguientes a c
+                while(true){
+                
+                    // Si el simbolo esta al final de la cadena, se calculan los
+                    // siguientes del simbolo que genera la produccion.
+                    if(sigPosicion == produccion.length()){
+                        productor = p.simbolo;
+                        // evitamos el ciclamiento con producciones del tipo:
+                        // S -> abS
+                        if(!productor.equals(c)){
+                            // Se agregan los siguientes del productor
+                            siguientes.addAll(obtenerSiguientes(productor));
+                            conjuntoSiguientes.put(c, siguientes);
                         }
+                        break;
                     }
-                    else if(obtenerPrimeros(posiblePrimero).contains('Ø')){
-                        obtenerPrimeros(p.produccion.charAt(index));
-                        index++;
+                
+                    // Si el simbolo no es el útimo elemento en la cadena, se
+                    // calculan los primeros del simbolo que le sigue.
+                    simboloProduccion = produccion.charAt(sigPosicion);
+                    Set<Character> primerosDeS = obtenerPrimeros(simboloProduccion);
+                
+                    // Si los primeros del siguiente no contienen a epsilon, 
+                    // se agregan los elementos al conjunto
+                    if(!primerosDeS.contains(EPSILON)){
+                        siguientes.addAll(primerosDeS);
+                        conjuntoSiguientes.put(c, siguientes);
+                        break;
                     }
-                }
-                else if(posiblePrimero == 'Ø'){
-                    primerosDeC.primeros.add(posiblePrimero);
-                    res.add(posiblePrimero);
-                }
-            }
-        }
-        if(noTerminales.contains(c))
-            primeros.add(primerosDeC);
-        return res;
-    }
-
-    public boolean yaSeCalculoPrimeros(Character c){
-        int i = 0;
-        for (PS p: primeros) {
-            if(p.simbolo == c){
-                aux = p;
-                return true;
-            }
-            i++;
-        }
-        return false;
-    }
-
-    public boolean yaSeCalculoSiguientes(Character c){
-        int i = 0;
-        for (PS s: siguientes) {
-            if(s.simbolo == c){
-                aux = s;
-                return true;
-            }
-            i++;
-        }
-        return false;
-    }
-
-    public Set<Character> obtenerSiguientes(Character c){
-        PS siguientesDeC = new PS();
-        char posibleSiguiente;
-        Set<Character> res = new HashSet<Character>();
-        siguientesDeC.simbolo = c;
-        if(c == axioma){
-            res.add('$');
-            siguientesDeC.siguientes.add('$');
-        }
-        if(yaSeCalculoSiguientes(c)) {
-            res.addAll(aux.siguientes);
-            siguientesDeC.siguientes.addAll(aux.siguientes);
-        }
-        else{
-            for (Produccion p: reglasDeProduccion) {
-                System.out.println(listaDeEspera.toString());
-                System.out.println(p.produccion);
-                if(!listaDeEspera.contains(c)){
-                    listaDeEspera.add(c);
-                    for(int i = 0; i < p.produccion.length(); i++){
-                        if(c == p.produccion.charAt(i)){
-                            if(i == p.produccion.length()-1){
-                                if(yaSeCalculoSiguientes(p.simbolo)){
-                                    res.addAll(aux.siguientes);
-                                    siguientesDeC.siguientes.addAll(aux.siguientes);
-                                }
-                                else{
-                                    res.addAll(obtenerSiguientes(p.simbolo));
-                                    siguientesDeC.siguientes.addAll(obtenerSiguientes(p.simbolo));
-                                }
-                            }
-                            else{
-                                boolean flag = false;
-                                for(int j = i+1; j < p.produccion.length(); j++){
-                                    if(!obtenerPrimeros(p.produccion.charAt(j)).contains('Ø')){
-                                        res.addAll(aux.primeros);
-                                        siguientesDeC.siguientes.addAll(aux.primeros);
-                                        flag = true;
-                                        break;
-                                    }
-                                }
-                                if(flag){
-                                    res.add('Ø');
-                                    siguientesDeC.siguientes.add('Ø');
-                                }
-
-                            }
-                        }
-                    }
-                }
-                try{
-                    listaDeEspera.remove(c);
-                }
-                catch(Exception e){
-                    System.out.println(e.toString());
+                
+                    // De lo contrario, se agregan sin incluir a epsilon, y se 
+                    // siguen buscando siguientes en las proximas posiciones.
+                    siguientes.addAll(primerosDeS);
+                    siguientes.remove(EPSILON);
+                    conjuntoSiguientes.put(c, siguientes);
+                    sigPosicion++;
                 }
             }
         }
-
-        siguientes.add(siguientesDeC);
-        return res;
+        return siguientes;
     }
 }
