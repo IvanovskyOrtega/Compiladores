ff262b46465c6a0c5ae7521211a515b932598881
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-12-05 14:48:50 -0600

    Creo que aun no esta bien la tabla

--- Practica-6-LL1/LL1.java
+++ Practica-6-LL1/LL1.java
@@ -1,27 +1,73 @@
 package ll1;
 
-public class LL1 extends Funciones{
-    public static void main(String[] args) {
-        Funciones f = new Funciones();
-        f.leerReglasDeProduccion();
-        System.out.println("Axioma: "+f.axioma);
-        for (Character c: f.noTerminales) {
-            f.obtenerPrimeros(c);
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LL1{
+    
+    LL1(){}
+    
+    Map<Character, Object> obtenerTabla(Funciones f){
+        Map<Character, Object> tabla = new HashMap<Character,Object>();
+        Character productor;
+        Character simboloProduccion;
+        String produccion;
+        Set<Character> primerosSet = new HashSet<Character>();
+        ArrayList<Character> primerosArray;
+        Set<Character> siguientesSet = new HashSet<Character>();
+        ArrayList<Character> siguientesArray;
+        ArrayList<Produccion> producciones = new ArrayList<Produccion>();
+        Map<Character, ArrayList<Produccion>> entradaTabla = new HashMap<Character, ArrayList<Produccion>>();
+        for(Produccion p : f.reglasDeProduccion){
+            productor = p.simbolo;
+            produccion = p.produccion;
+            simboloProduccion = produccion.charAt(0);
+            primerosSet = f.obtenerPrimeros(simboloProduccion);
+            primerosArray = new ArrayList<Character>(primerosSet);
+            for (int i = 0; i < primerosArray.size(); i++) {
+                if(tabla.containsKey(productor)){
+                    entradaTabla = (Map)tabla.get(productor);
+                    if(entradaTabla.containsKey(primerosArray.get(i))){
+                        entradaTabla.get(primerosArray.get(i)).add(p);
+                        tabla.replace(productor, entradaTabla);
+                    }
+                    else{
+                        producciones.add(p);
+                        entradaTabla.put(primerosArray.get(i),producciones);
+                    }
+                }
+                else{
+                    producciones.add(p);
+                    entradaTabla.put(primerosArray.get(i),producciones);
+                    tabla.put(productor,entradaTabla);
+                }
+            }
+            if(primerosSet.contains(f.EPSILON)){
+                siguientesSet = f.obtenerPrimeros(productor);
+                siguientesArray = new ArrayList<Character>(siguientesSet);
+                for (int i = 0; i < siguientesArray.size(); i++) {
+                    if(tabla.containsKey(productor)){
+                    entradaTabla = (Map)tabla.get(productor);
+                    if(entradaTabla.containsKey(siguientesArray.get(i))){
+                        entradaTabla.get(siguientesArray.get(i)).add(p);
+                        tabla.replace(productor, entradaTabla);
+                    }
+                    else{
+                        producciones.add(p);
+                        entradaTabla.put(siguientesArray.get(i),producciones);
+                    }
+                }
+                else{
+                    producciones.add(p);
+                    entradaTabla.put(siguientesArray.get(i),producciones);
+                    tabla.put(productor,entradaTabla);
+                }
+                }
+            }
         }
-        for(Character c : f.noTerminales){
-            if(f.conjuntoPrimeros.containsKey(c)){
-                System.out.println("Primeros de "+c+":");
-                System.out.println(f.conjuntoPrimeros.get(c).toString());
-            };
-        }
-        for (Character c: f.noTerminales) {
-            f.obtenerSiguientes(c);
-        }
-        for(Character c: f.noTerminales){
-            if(f.conjuntoSiguientes.containsKey(c)){
-                System.out.println("Siguientes de "+c+":");
-                System.out.println(f.conjuntoSiguientes.get(c).toString());
-            };
-        }
+        return tabla;
     }
 }
