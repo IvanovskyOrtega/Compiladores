0bb973eaccaf8979b0ab763cf03e5764628587fe
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-12-16 00:56:40 -0600

    Se acabó :,D

---
+++ Proyecto/analizador.py
@@ -0,0 +1,33 @@
+from ply.yacc import yacc
+from ply.lex import lex
+from guitarYacc import parser
+from guitarLex import lexer
+from guitarYacc import codigo_intermedio
+import sys
+
+class AnalizadorSintactico:
+
+    def __init__(self):
+        self.lexer = lexer
+        self.parser = parser
+
+    def analizar(self,nombreArchivo):
+        with open(nombreArchivo,'r') as f:
+            input = f.read()
+            #print(input)
+            print(parser.parse(input,lexer=lexer))
+        codigo_intermedio.write('MyMIDI.writeFile(archivoMIDI)')
+
+
+class Main:
+
+    def iniciar(self):
+        analizador_sintactico = AnalizadorSintactico()
+        print(str(sys.argv[1]))
+        try:
+            analizador_sintactico.analizar(str(sys.argv[1]))
+        except:
+            print('Intenta con archivo valido')
+
+main = Main()
+main.iniciar()
