4f7ef6dbda183ccf415a69294dd273e4be02819f
Ivan Ortega Victoriano <iortegav1500@alumno.ipn.mx>
2017-12-15 22:40:58 -0600

    Ya solo falta el código intermedio :)

---
+++ analizador.py
@@ -0,0 +1,31 @@
+from ply.yacc import yacc
+from ply.lex import lex
+from guitarYacc import parser
+from guitarLex import lexer
+import sys
+
+class AnalizadorSintactico:
+
+    def __init__(self):
+        self.lexer = lexer
+        self.parser = parser
+
+    def analizar(self,nombreArchivo):
+        with open(nombreArchivo,'r') as f:
+            input = f.read()
+            #print(input)
+            print(parser.parse(input,lexer=lexer))
+
+
+class Main:
+
+    def iniciar(self):
+
+        analizador_sintactico = AnalizadorSintactico()
+        try:
+            analizador_sintactico.analizar(str(sys.argv[1]))
+        except:
+            print('Intenta con archivo valido')
+
+main = Main()
+main.iniciar()
